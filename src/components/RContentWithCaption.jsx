ddd9dd200a813e18e6654d4e591c6d17403cca6d
Matthew McCall <matthewm3004@icloud.com>
2020-12-22 13:07:41 -0500

    Added new components

---
+++ src/components/RContentWithCaption.jsx
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react'
+import { ThemeContext } from '../AppDelegate'
+import VCenter from './VCenter'
+
+export default function RContentWithCaption(props) {
+
+    const theme = useContext(ThemeContext)
+
+    const styles = {
+        color: theme.style.palette.themePrimary,
+    }
+
+    return (
+        <div className="container-fluid">
+            <div className="row vh-100">
+                <div className="col">
+                    <VCenter>
+                        {props.title != undefined && <h1 style={styles} className="display-1 bladerunner-font">{props.title}</h1>}
+                        <p className="lead">{props.caption}</p>
+                    </VCenter>
+                </div>
+                <div className="col">
+                    {props.children}
+                </div>
+            </div>
+        </div>
+    )
+}
