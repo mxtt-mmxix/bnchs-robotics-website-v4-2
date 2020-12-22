c5139e389c64c324193f6c109ce5cff7b70bccf4
Matthew McCall <matthewm3004@icloud.com>
2020-12-22 13:08:18 -0500

    Altered theming of the navigation bar.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,14 +8,18 @@ export default function NavBar(props) {
 
     const theme = useContext(ThemeContext)
 
-    const styles = {
+    const navStyles = {
         background: theme.style.palette.white,
     }
 
+    const styles = {
+        color: theme.style.palette.themePrimary,
+    }
+
     return (
-        <nav style={styles} className={`navbar navbar-expand-lg fixed-top ${theme.mode === "light" ? "navbar-light" : "navbar-dark"} `}>
+        <nav style={navStyles} className={`d-none d-md-block navbar navbar-expand-lg fixed-top ${theme.mode === "light" ? "navbar-light" : "navbar-dark"} `}>
             <div className="container">
-                <NavLink className="navbar-brand" to="/">Website</NavLink>
+                <NavLink style={styles} className="navbar-brand bladerunner-font" to="/">The Sentinels</NavLink>
                 <Toggle styles={theme.style} label="Theme" inlineLabel onText="Dark" offText="Light" onChange={theme.updateTheme} />
             </div>
         </nav>
